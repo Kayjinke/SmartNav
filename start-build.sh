27961af2864c5a637a2bcba47130a245f2c2dfb5
yanshucheng <42833028@qq.com>
2019-08-01 09:17:10 +0800

    add startbuidl script

---
+++ start-build.sh
@@ -0,0 +1,51 @@
+#! /bin/bash
+
+_fullname=`pwd`
+_dirname=`dirname $_fullname`
+_basename=`basename $_fullname`
+
+#echo "Fullname is $_fullname"
+#echo "Dirname is $_dirname"
+#echo "Basename is $_basename"
+
+
+#echo "_buildout_fullname is $_buildout_fullname"
+
+arch=x86_64
+cleanflag=0
+
+for arg in $*
+do
+    echo "arg $arg"
+    if [ "$arg"x = "-c"x ]
+       then
+           cleanflag=1
+    fi
+    if [ "$arg"x = "-arm"x ]
+       then
+           arch=arm
+    fi
+done
+
+#echo "arch is " $arch
+
+
+_buildout_basename=$_basename-$arch-build
+_buildout_fullname=$_dirname/$_buildout_basename
+
+if [ "$cleanflag"x = "1"x ]
+    then
+        rm -rf $_buildout_fullname
+        exit 0
+fi     
+
+
+cd ..
+if [ ! -d $_buildout_fullname ]
+   then
+      mkdir $_buildout_fullname
+fi
+
+cd $_buildout_fullname
+cmake $_fullname -DARCH=$arch
+make
