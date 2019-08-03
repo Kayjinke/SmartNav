986f4dfb34c2a79d0579b5cc4cffe959542593de
shengwangsheng <shenwangsheng@163.com>
2019-08-03 14:55:08 +0800

    ss

--- src/navcore/position/PositionStubImpl.cpp
+++ src/navcore/position/PositionStubImpl.cpp
@@ -1,9 +1,26 @@
 // PositionStubImpl.cpp
 #include "PositionStubImpl.hpp"
+#include <iostream>
+#include <unistd.h>
+using namespace std;
 
-PositionStubImpl::PositionStubImpl()  { }
+bool PositionStubImpl::m_IsStart=false;
+
+PositionStubImpl::PositionStubImpl()
+{
+}
+
 PositionStubImpl::~PositionStubImpl() { }
 
+
+void PositionStubImpl::demo_thread_func()
+{
+    while(m_IsStart)
+    {
+         cout << "I am working" << endl;
+         sleep(1);
+    }
+}
 
 void PositionStubImpl::setRoute(const std::shared_ptr<CommonAPI::ClientId> _client, Position::Shapepoints _route, setRouteReply_t _reply){
 
@@ -11,9 +28,15 @@ void PositionStubImpl::setRoute(const std::shared_ptr<CommonAPI::ClientId> _clie
 
 };
 
-void PositionStubImpl::startDemo(const std::shared_ptr<CommonAPI::ClientId> _client, startDemoReply_t _reply){
+void PositionStubImpl::startDemo(const std::shared_ptr<CommonAPI::ClientId> _client, startDemoReply_t _reply)
+{
+       m_IsStart = true;
+       m_DemoThread = new std::thread(PositionStubImpl::demo_thread_func);
 };
 
-void PositionStubImpl::stopDemo(const std::shared_ptr<CommonAPI::ClientId> _client, stopDemoReply_t _reply){
+void PositionStubImpl::stopDemo(const std::shared_ptr<CommonAPI::ClientId> _client, stopDemoReply_t _reply)
+{
+	   m_IsStart = false;
+	
 };
 
